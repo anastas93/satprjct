f95627697384a469e1e614ed44b124684e641aaa
anastas93 <smanastas93@gmail.com>
2025-08-20 17:47:18 +0500

    Добавлен include guard для qos

--- libs/qos.h
+++ libs/qos.h
@@ -1,6 +1,11 @@
 #pragma once
+#ifndef LIBS_QOS_H
+#define LIBS_QOS_H
+
 #include <stdint.h>
 
 // Перечисления уровней приоритета QoS и режимов планировщика
 enum class Qos : uint8_t { High=0, Normal=1, Low=2 };
 enum class QosMode : uint8_t { Strict=0, Weighted421=1 };
+
+#endif // LIBS_QOS_H
